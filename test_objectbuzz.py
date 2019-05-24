7e7ab4d0fb5a1b52eb724b55bc6ebff53395f147
Eric Hopper <eric.hopper@servicenow.com>
2019-05-23 21:30:55 -0700

    Step 4 - Failing test.

--- test_objectbuzz.py
+++ test_objectbuzz.py
@@ -7,3 +7,6 @@ def test_fizz_1():
 
 def test_fizz_1_2():
     assert objectbuzz.FizzBuzzer(1) != objectbuzz.FizzBuzzer(2)
+
+def test_fizz_3_6():
+    assert objectbuzz.FizzBuzzer(3) == objectbuzz.FizzBuzzer(6)
